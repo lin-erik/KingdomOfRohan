3fe6431c65e55f8fa1f29819094d4d70a063fa02
parkermuir <parker.muir@gmail.com>
2018-07-16 19:17:32 -0400

    Created landing page, components folder
    -index.html should render bundle.js script on load
    -included Bulma CSS file in index.html for now
    -index.jsx creates main App componenet and renders to DOM

---
+++ client/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+  render() {
+    <div>
+      <h1 className="title is-1">Kingdom of Rohan and the Movie Moods</h1>
+      <section className="section">
+
+        {/* Search component */}
+
+      </section>
+    </div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
