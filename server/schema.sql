f6d10770dcf89c0513d3149e85e067801aa412ab
Kiernan Hahn <kiernanhahn@gmail.com>
2018-07-17 14:29:58 -0400

    Adjusted the Schema with a moodvies.History table instead of individual tables for users

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,13 @@
-DROP SCHEMA IF EXISTS 'moodvies'
+DROP SCHEMA IF EXISTS moodvies
 
 -- ---
 -- Globals
 -- ---
-CREATE SCHEMA IF NOT EXISTS 'moodvies' DEFAULT CHARACTER SET utf8;
-USE 'moodvies';
+CREATE SCHEMA IF NOT EXISTS moodvies DEFAULT CHARACTER SET utf8;
+USE moodvies;
 
 
-CREATE TABLE IF NOT EXISTS 'moodvies'.'Movies' (
+CREATE TABLE IF NOT EXISTS moodvies.Movies (
 		movieId int NOT NULL AUTO_INCREMENT,
 		movieName varchar (100),
 		moviePosterURL varchar (255),
@@ -34,11 +34,31 @@ CREATE TABLE IF NOT EXISTS 'moodvies'.'Movies' (
 )
 
 
-CREATE TABLE IF NOT EXISTS 'moodvies'.'Users' (
+CREATE TABLE IF NOT EXISTS moodvies.Users (
 		userId int NOT NULL AUTO_INCREMENT,
 		username varchar (100),
 		password varchar (100)
 		PRIMARY KEY (userId);
+)
+
+CREATE TABLE IF NOT EXISTS moodvies.History (
+  userId int NOT NULL,
+  movieId int NOT NULL,
+  tag1 varchar (30),
+  tag2 varchar (30),
+  tag3 varchar (30),
+  CONSTRAINT userId
+    FOREIGN KEY (userId)
+    REFERENCES moodvies.Users
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+  CONSTRAINT movieId
+    FOREIGN KEY (movieId)
+    REFERENCES moodvies.Movies
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    
+
 )
 -- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- SET FOREIGN_KEY_CHECKS=0;
