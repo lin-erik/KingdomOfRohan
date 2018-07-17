f855d7e349615a255dac0826f5a73f0b452992f0
Martin <MartinGlyer@gmail.com>
2018-07-17 16:05:49 -0400

    alters exports

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -44,20 +44,6 @@ let MovieSchema = mongoose.Schema ({
 })
 let User = mongoose.model('User', UserSchema);
 let Movie = mongoose.model ('Movie', MovieSchema);
-module.exports = {
-  
-  
-  // Posts to the Users Table, as well as creates a new Table named after the username that holds the movies watched by the user and the
-  users: {
-    post: function (req, res) {
-      User.find({username: req.username}, function (err, docs) {
-        
-      })
-      //first query checks to see if the new username is already present in the database;
-      
-    }
-    get: function (req, res) {
-
-    }
-  }
-}
+module.exports.db = db
+module.exports.User = User
+module.exports.Movie = Movie
