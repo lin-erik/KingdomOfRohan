67cc81457f45057af2c95e3c62e037e3390e447a
parkermuir <parker.muir@gmail.com>
2018-07-17 15:44:49 -0400

    Frank's Profile_Search
    -had some git problems so doing this from my account
    -New profile search component rendering and fully functional

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Login_Signup from './components/login_signup.jsx';
 import GlobalSearch from './components/GlobalSearch.jsx';
+import Profile_Search from './components/Profile_Search.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -48,7 +49,7 @@ class App extends React.Component {
       return (
         <div>
           <h1 className="title is-1">Kingdom of Rohan and the Movie Moods</h1>
-          {/*render out the profile page*/}
+          <Profile_Search />
           {this.state.profileSearched ?
             <div>
               {/*render out the movie*/}
