41693ead0235966e5ba4635879db7c5169a0ab88
parkermuir <parker.muir@gmail.com>
2018-07-17 12:55:27 -0400

    Creates the MovieCard component

--- client/components/MovieCard.jsx
+++ client/components/MovieCard.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 //from props this needs a dynamic img src, title, year and (conditional) mood tags
 var MovieCard = (props) => {
   return (
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-4by3">
           <img src="https://vignette.wikia.nocookie.net/lotr/images/8/8b/King_of_rohan.jpg/revision/latest?cb=20140426175218" alt="Placeholder image" />
         </figure>
       </div>
-      <div class="card-content">
-        <p class="title">Movie Title</p>
-        <p class="subtitle">2008</p>
+      <div className="card-content">
+        <p className="title">Movie Title</p>
+        <p className="subtitle">2008</p>
         <p>Mood tags will go here</p>
       </div>
     </div>
