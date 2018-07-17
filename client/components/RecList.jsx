500a56b430de7b88e63324ff63f0fc3d6cfa0d40
Kiernan Hahn <kiernanhahn@gmail.com>
2018-07-17 19:59:44 -0400

    Almost completed getUserMovies function

--- client/components/RecList.jsx
+++ client/components/RecList.jsx
@@ -5,17 +5,38 @@ var recList extends React.component {
     this.state = (
       watchedMovies: [],
       queuedMovies: [],
-      userTags: []
+      userTags:  [{mood: null, count: 0},
+                  {mood: null, count: 0},
+                  {mood: null, count: 0}]
     )
+    
+    //returns an array of the movies the user has tagged and watched
+    //and then parses that information into the state as watchMovies
+    //and userTags
     function getUserMovies {
       return axios.get('/User/',
-             params: {
+              params: {
               username: props.username;
-              }).then(function (response) {
-                let userTags = {};
+              }).then (function (response) {
+                this.state.watchedMovies = response;
+                var userTagTotals = {};
                 for (var i = 0; i < response.length; i++) {
-                  if (userTags)
+                  userTagTotals[response[i].tag1] = userTagTotals[response[i].tag1] ? userTagTotals[response[i].tag1]++ : 1;
+                  if (response[i].tag2) {
+                   userTagTotals[response[i].tag2] = userTagTotals[response[i].tag2] ? userTagTotals[response[i].tag2]++ : 1; 
+                  }
+                  if (response[i].tag3) {
+                   userTagTotals[response[i].tag3] = userTagTotals[response[i].tag3] ? userTagTotals[response[i].tag3]++ : 1; 
+                  }
                 }
+                return userTagTotals;
+              }).then (function (response) {
+                for (var i = 0; i < response.length; i++) {
+                  for (var j = 0; j < 3; j++) {
+                    if ()
+                  }
+                }
+                
               })
     }
   }
