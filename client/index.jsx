e0ebc40fc17a0936de5f9f50fa8430f89483061d
parkermuir <parker.muir@gmail.com>
2018-07-17 17:23:04 -0400

    Implemented react react_router
    -Add to package json react router dom
    -Create Nav bar component
    -Refactor render method of index.js for react router

--- client/index.jsx
+++ client/index.jsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 import Login_Signup from './components/login_signup.jsx';
 import GlobalSearch from './components/GlobalSearch.jsx';
-
 import Profile_Search from './components/Profile_Search.jsx';
+import Nav from './components/Nav.jsx';
+import {BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      loggedIn: false,
-      profilePage: true,
-      profileSearched: false
+      loggedIn: false
     };
 
     this.handleSignUp = this.handleSignUp.bind(this);
@@ -38,36 +37,18 @@ class App extends React.Component {
   }
 
   render() {
-    if (!this.state.loggedIn) {
-
-      return (
-        <div>
-          <h1 className="title is-1">Kingdom of Rohan and the Movie Moods</h1>
-          <Login_Signup signup={this.handleSignUp} login={this.handleLogin} />
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route path="/login" render={() => <Login_Signup signup={this.handleSignUp} login={this.handleLogin} />} />
+            <Route path="/profile" render={() => <Profile_Search />} />
+            <Route path="/global" render={() => <GlobalSearch />} />
+          </Switch>
         </div>
-      );
-    } else if (this.state.loggedIn && this.state.profilePage) {
-      return (
-        <div>
-          <h1 className="title is-1">Kingdom of Rohan and the Movie Moods</h1>
-
-          <Profile_Search />
-          {this.state.profileSearched ?
-            <div>
-              {/*render out the movie*/}
-            </div>
-            : null
-          }
-        </div>
-      );
-    } else if (this.state.loggedIn && !this.state.profilePage) {
-      return (
-        <div>
-          <h1 className="title is-1">Kingdom of Rohan and the Movie Moods</h1>
-          <GlobalSearch />
-        </div>
-      );
-    }
+      </BrowserRouter>
+    );
   }
 }
 
