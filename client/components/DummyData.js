f64bad3841a9a29f0a968fe237e4d28b63b4d76f
parkermuir <parker.muir@gmail.com>
2018-07-18 00:02:33 -0400

    Results list component work

---
+++ client/components/DummyData.js
@@ -0,0 +1,185 @@
+exports.dummyData = [
+  {
+    "vote_count": 2973,
+    "id": 268,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "Batman",
+    "popularity": 13.38,
+    "poster_path": "/kBf3g9crrADGMc2AMAMlLBgSm2h.jpg",
+    "original_language": "en",
+    "original_title": "Batman",
+    "genre_ids": [
+      14,
+      28
+    ],
+    "backdrop_path": "/2blmxp2pr4BhwQr74AdCfwgfMOb.jpg",
+    "adult": false,
+    "overview": "The Dark Knight of Gotham City begins his war on crime with his first major enemy being the clownishly homicidal Joker, who has seized control of Gotham's underworld.",
+    "release_date": "1989-06-23"
+  },
+  {
+    "vote_count": 292,
+    "id": 2661,
+    "video": false,
+    "vote_average": 6.2,
+    "title": "Batman",
+    "popularity": 5.733,
+    "poster_path": "/udDVJXtAFsQ8DimrXkVFqy4DGEQ.jpg",
+    "original_language": "en",
+    "original_title": "Batman",
+    "genre_ids": [
+      10751,
+      12,
+      35,
+      878,
+      80
+    ],
+    "backdrop_path": "/5gcdof2PKH1emllBdN1VXU706IP.jpg",
+    "adult": false,
+    "overview": "The Dynamic Duo faces four super-villains who plan to hold the world for ransom with the help of a secret invention that instantly dehydrates people.",
+    "release_date": "1966-07-30"
+  },
+  {
+    "vote_count": 9819,
+    "id": 272,
+    "video": false,
+    "vote_average": 7.6,
+    "title": "Batman Begins",
+    "popularity": 23.741,
+    "poster_path": "/dr6x4GyyegBWtinPBzipY02J2lV.jpg",
+    "original_language": "en",
+    "original_title": "Batman Begins",
+    "genre_ids": [
+      28,
+      80,
+      18
+    ],
+    "backdrop_path": "/65JWXDCAfwHhJKnDwRnEgVB411X.jpg",
+    "adult": false,
+    "overview": "Driven by tragedy, billionaire Bruce Wayne dedicates his life to uncovering and defeating the corruption that plagues his home, Gotham City.  Unable to work within the system, he instead creates a new identity, a symbol of fear for the criminal underworld - The Batman.",
+    "release_date": "2005-06-10"
+  },
+  {
+    "vote_count": 2038,
+    "id": 415,
+    "video": false,
+    "vote_average": 4.2,
+    "title": "Batman & Robin",
+    "popularity": 11.7,
+    "poster_path": "/79AYCcxw3kSKbhGpx1LiqaCAbwo.jpg",
+    "original_language": "en",
+    "original_title": "Batman & Robin",
+    "genre_ids": [
+      878,
+      28,
+      14,
+      35
+    ],
+    "backdrop_path": "/39jbxGwfR4wHBV7apo3JiNdHDHq.jpg",
+    "adult": false,
+    "overview": "Along with crime-fighting partner Robin and new recruit Batgirl, Batman battles the dual threat of frosty genius Mr. Freeze and homicidal horticulturalist Poison Ivy. Freeze plans to put Gotham City on ice, while Ivy tries to drive a wedge between the dynamic duo.",
+    "release_date": "1997-06-20"
+  },
+  {
+    "vote_count": 2397,
+    "id": 364,
+    "video": false,
+    "vote_average": 6.7,
+    "title": "Batman Returns",
+    "popularity": 10.452,
+    "poster_path": "/jX5THE1yW3zTdeD9dupcIyQvKiG.jpg",
+    "original_language": "en",
+    "original_title": "Batman Returns",
+    "genre_ids": [
+      28,
+      14
+    ],
+    "backdrop_path": "/reQewz6KndV7Js2mfLfaaX8Rj0T.jpg",
+    "adult": false,
+    "overview": "Having defeated the Joker, Batman now faces the Penguin - a warped and deformed individual who is intent on being accepted into Gotham society. Crooked businessman Max Schreck is coerced into helping him become Mayor of Gotham and they both attempt to expose Batman in a different light. Selina Kyle, Max's secretary, is thrown from the top of a building and is transformed into Catwoman - a mysterious figure who has the same personality disorder as Batman. Batman must attempt to clear his name, all the time deciding just what must be done with the Catwoman.",
+    "release_date": "1992-06-19"
+  },
+  {
+    "vote_count": 2130,
+    "id": 414,
+    "video": false,
+    "vote_average": 5.3,
+    "title": "Batman Forever",
+    "popularity": 9.741,
+    "poster_path": "/eTMrHEhlFPHNxpqGwpGGTdAa0xV.jpg",
+    "original_language": "en",
+    "original_title": "Batman Forever",
+    "genre_ids": [
+      28,
+      80,
+      14
+    ],
+    "backdrop_path": "/bbaiw9VmrHXY3mhCDpj5XB2I5We.jpg",
+    "adult": false,
+    "overview": "The Dark Knight of Gotham City confronts a dastardly duo: Two-Face and the Riddler. Formerly District Attorney Harvey Dent, Two-Face believes Batman caused the courtroom accident which left him disfigured on one side. And Edward Nygma, computer-genius and former employee of millionaire Bruce Wayne, is out to get the philanthropist; as The Riddler. Former circus acrobat Dick Grayson, his family killed by Two-Face, becomes Wayne's ward and Batman's new partner Robin.",
+    "release_date": "1995-06-16"
+  },
+  {
+    "vote_count": 110,
+    "id": 485942,
+    "video": false,
+    "vote_average": 6,
+    "title": "Batman Ninja",
+    "popularity": 13.81,
+    "poster_path": "/kZIrg87DiU1xKqCNcEV6LMfj3pS.jpg",
+    "original_language": "ja",
+    "original_title": "ニンジャバットマン",
+    "genre_ids": [
+      28,
+      16
+    ],
+    "backdrop_path": "/cDbJAqGIsnpJfiNiYNBQYQs0ZWY.jpg",
+    "adult": false,
+    "overview": "Gorilla Grodd's time displacement machine transports many of Batman's worst enemies to feudal Japan —along with the Dark Knight and a few of his allies. The villains take over the forms of the feudal lords that rule the divided land, with the Joker taking the lead among the warring factions. As his traditional high-tech weaponry is exhausted almost immediately, Batman must rely on his intellect and his allies —including Catwoman and the extended Bat-family— to restore order to the land, and return to present-day Gotham City.",
+    "release_date": "2018-04-24"
+  },
+  {
+    "vote_count": 2213,
+    "id": 324849,
+    "video": false,
+    "vote_average": 7.2,
+    "title": "The Lego Batman Movie",
+    "popularity": 11.789,
+    "poster_path": "/1pHOqpdCYNmtRVJs6pGKQKttrPm.jpg",
+    "original_language": "en",
+    "original_title": "The Lego Batman Movie",
+    "genre_ids": [
+      28,
+      16,
+      35,
+      10751,
+      14
+    ],
+    "backdrop_path": "/jqnbac3oind5VOcAN0mTXyapOGh.jpg",
+    "adult": false,
+    "overview": "In the irreverent spirit of fun that made “The Lego Movie” a worldwide phenomenon, the self-described leading man of that ensemble—Lego Batman—stars in his own big-screen adventure. But there are big changes brewing in Gotham, and if he wants to save the city from The Joker’s hostile takeover, Batman may have to drop the lone vigilante thing, try to work with others and maybe, just maybe, learn to lighten up.",
+    "release_date": "2017-02-08"
+  },
+  {
+    "vote_count": 196,
+    "id": 408648,
+    "video": false,
+    "vote_average": 5.8,
+    "title": "Batman and Harley Quinn",
+    "popularity": 9.34,
+    "poster_path": "/dPsjte846EcEWQpfMmGHrTWIJd8.jpg",
+    "original_language": "en",
+    "original_title": "Batman and Harley Quinn",
+    "genre_ids": [
+      12,
+      28,
+      16,
+      878
+    ],
+    "backdrop_path": "/2JVnUtyUKbQm165cXUF9r5iVPDo.jpg",
+    "adult": false,
+    "overview": "Batman and Nightwing are forced to team with the Joker's sometimes-girlfriend Harley Quinn to stop a global threat brought about by Poison Ivy and Jason Woodrue, the Floronic Man.",
+    "release_date": "2017-08-14"
+  }
+];
