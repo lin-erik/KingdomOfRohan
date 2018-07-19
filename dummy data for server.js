d67fb045f5a4ff5c9930e265fd9e94628bec6aa2
Martin <MartinGlyer@gmail.com>
2018-07-19 11:03:33 -0400

    grabs a bunch of dummy data from movie DB

---
+++ dummy data for server.js
@@ -0,0 +1,369 @@
+let data = [
+    {
+    "vote_count": 1926,
+    "id": 351286,
+    "video": false,
+    "vote_average": 6.6,
+    "title": "Jurassic World: Fallen Kingdom",
+    "popularity": 239.844,
+    "poster_path": "/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg",
+    "original_language": "en",
+    "original_title": "Jurassic World: Fallen Kingdom",
+    "genre_ids": [
+      28,
+      12,
+      878
+    ],
+    "backdrop_path": "/gBmrsugfWpiXRh13Vo3j0WW55qD.jpg",
+    "adult": false,
+    "overview": "Several years after the demise of Jurassic World, a volcanic eruption threatens the remaining dinosaurs on the island of Isla Nublar. Claire Dearing, the former park manager and founder of the Dinosaur Protection Group, recruits Owen Grady to help prevent the extinction of the dinosaurs once again.",
+    "release_date": "2018-06-06"
+  },
+  {
+    "vote_count": 560,
+    "id": 363088,
+    "video": false,
+    "vote_average": 7.1,
+    "title": "Ant-Man and the Wasp",
+    "popularity": 180.247,
+    "poster_path": "/rv1AWImgx386ULjcf62VYaW8zSt.jpg",
+    "original_language": "en",
+    "original_title": "Ant-Man and the Wasp",
+    "genre_ids": [
+      28,
+      12,
+      14,
+      35,
+      878
+    ],
+    "backdrop_path": "/6P3c80EOm7BodndGBUAJHHsHKrp.jpg",
+    "adult": false,
+    "overview": "As Scott Lang awaits expiration of his term of house detention, Hope van Dyne and Dr. Hank Pym involve him in a scheme to rescue Mrs. van Dyne from the micro-universe into which she has fallen, while two groups of schemers converge on them with intentions of stealing Dr. Pym's inventions.",
+    "release_date": "2018-07-04"
+  },
+  {
+    "vote_count": 7325,
+    "id": 284053,
+    "video": false,
+    "vote_average": 7.5,
+    "title": "Thor: Ragnarok",
+    "popularity": 144.24,
+    "poster_path": "/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
+    "original_language": "en",
+    "original_title": "Thor: Ragnarok",
+    "genre_ids": [
+      28,
+      12,
+      35
+    ],
+    "backdrop_path": "/kaIfm5ryEOwYg8mLbq8HkPuM1Fo.jpg",
+    "adult": false,
+    "overview": "Thor is on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.",
+    "release_date": "2017-10-25"
+  },
+  {
+    "vote_count": 5696,
+    "id": 299536,
+    "video": false,
+    "vote_average": 8.4,
+    "title": "Avengers: Infinity War",
+    "popularity": 131.175,
+    "poster_path": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+    "original_language": "en",
+    "original_title": "Avengers: Infinity War",
+    "genre_ids": [
+      12,
+      878,
+      14,
+      28
+    ],
+    "backdrop_path": "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+    "adult": false,
+    "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
+    "release_date": "2018-04-25"
+  },
+  {
+    "vote_count": 1125,
+    "id": 260513,
+    "video": false,
+    "vote_average": 7.7,
+    "title": "Incredibles 2",
+    "popularity": 115.202,
+    "poster_path": "/x1txcDXkcM65gl7w20PwYSxAYah.jpg",
+    "original_language": "en",
+    "original_title": "Incredibles 2",
+    "genre_ids": [
+      28,
+      12,
+      16,
+      10751
+    ],
+    "backdrop_path": "/mabuNsGJgRuCTuGqjFkWe1xdu19.jpg",
+    "adult": false,
+    "overview": "Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.",
+    "release_date": "2018-06-14"
+  },
+  {
+    "vote_count": 1301,
+    "id": 427641,
+    "video": false,
+    "vote_average": 6.1,
+    "title": "Rampage",
+    "popularity": 109.676,
+    "poster_path": "/3gIO6mCd4Q4PF1tuwcyI3sjFrtI.jpg",
+    "original_language": "en",
+    "original_title": "Rampage",
+    "genre_ids": [
+      28,
+      12,
+      878,
+      14
+    ],
+    "backdrop_path": "/wrqUiMXttHE4UBFMhLHlN601MZh.jpg",
+    "adult": false,
+    "overview": "Primatologist Davis Okoye shares an unshakable bond with George, the extraordinarily intelligent, silverback gorilla who has been in his care since birth.  But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size.  To make matters worse, it’s soon discovered there are other similarly altered animals.  As these newly created alpha predators tear across North America, destroying everything in their path, Okoye teams with a discredited genetic engineer to secure an antidote, fighting his way through an ever-changing battlefield, not only to halt a global catastrophe but to save the fearsome creature that was once his friend.",
+    "release_date": "2018-04-12"
+  },
+  {
+    "vote_count": 2957,
+    "id": 333339,
+    "video": false,
+    "vote_average": 7.7,
+    "title": "Ready Player One",
+    "popularity": 95.577,
+    "poster_path": "/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg",
+    "original_language": "en",
+    "original_title": "Ready Player One",
+    "genre_ids": [
+      12,
+      878,
+      14
+    ],
+    "backdrop_path": "/q7fXcrDPJcf6t3rzutaNwTzuKP1.jpg",
+    "adult": false,
+    "overview": "When the creator of a popular video game system dies, a virtual contest is created to compete for his fortune.",
+    "release_date": "2018-03-28"
+  },
+  {
+    "vote_count": 2287,
+    "id": 337167,
+    "video": false,
+    "vote_average": 6,
+    "title": "Fifty Shades Freed",
+    "popularity": 93.801,
+    "poster_path": "/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg",
+    "original_language": "en",
+    "original_title": "Fifty Shades Freed",
+    "genre_ids": [
+      18,
+      10749
+    ],
+    "backdrop_path": "/9ywA15OAiwjSTvg3cBs9B7kOCBF.jpg",
+    "adult": false,
+    "overview": "Believing they have left behind shadowy figures from their past, newlyweds Christian and Ana fully embrace an inextricable connection and shared life of luxury. But just as she steps into her role as Mrs. Grey and he relaxes into an unfamiliar stability, new threats could jeopardize their happy ending before it even begins.",
+    "release_date": "2018-01-17"
+  },
+  {
+    "vote_count": 22,
+    "id": 353081,
+    "video": false,
+    "vote_average": 6,
+    "title": "Mission: Impossible - Fallout",
+    "popularity": 90.916,
+    "poster_path": "/80PWnSTkygi3QWWmJ3hrAwqvLnO.jpg",
+    "original_language": "en",
+    "original_title": "Mission: Impossible - Fallout",
+    "genre_ids": [
+      12,
+      28,
+      53
+    ],
+    "backdrop_path": "/5qxePyMYDisLe8rJiBYX8HKEyv2.jpg",
+    "adult": false,
+    "overview": "When an IMF mission ends badly, the world is faced with dire consequences. As Ethan Hunt takes it upon himself to fulfil his original briefing, the CIA begin to question his loyalty and his motives. The IMF team find themselves in a race against time, hunted by assassins while trying to prevent a global catastrophe.",
+    "release_date": "2018-07-25"
+  },
+  {
+    "vote_count": 256,
+    "id": 442249,
+    "video": false,
+    "vote_average": 6.2,
+    "title": "The First Purge",
+    "popularity": 86.341,
+    "poster_path": "/2slvblTroiT1lY9bYLK7Amigo1k.jpg",
+    "original_language": "en",
+    "original_title": "The First Purge",
+    "genre_ids": [
+      28,
+      27,
+      878,
+      53
+    ],
+    "backdrop_path": "/dnaitaoCh8MftfYEVnprcuYExZp.jpg",
+    "adult": false,
+    "overview": "To push the crime rate below one percent for the rest of the year, the New Founding Fathers of America test a sociological theory that vents aggression for one night in one isolated community. But when the violence of oppressors meets the rage of the others, the contagion will explode from the trial-city borders and spread across the nation.",
+    "release_date": "2018-07-04"
+  },
+  {
+    "vote_count": 2199,
+    "id": 447332,
+    "video": false,
+    "vote_average": 7.2,
+    "title": "A Quiet Place",
+    "popularity": 85.336,
+    "poster_path": "/nAU74GmpUk7t5iklEp3bufwDq4n.jpg",
+    "original_language": "en",
+    "original_title": "A Quiet Place",
+    "genre_ids": [
+      18,
+      27,
+      53,
+      878
+    ],
+    "backdrop_path": "/roYyPiQDQKmIKUEhO912693tSja.jpg",
+    "adult": false,
+    "overview": "A family is forced to live in silence while hiding from creatures that hunt by sound.",
+    "release_date": "2018-04-03"
+  },
+  {
+    "vote_count": 97,
+    "id": 400155,
+    "video": false,
+    "vote_average": 7,
+    "title": "Hotel Transylvania 3: Summer Vacation",
+    "popularity": 80.601,
+    "poster_path": "/gjAFM4xhA5vyLxxKMz38ujlUfDL.jpg",
+    "original_language": "en",
+    "original_title": "Hotel Transylvania 3: Summer Vacation",
+    "genre_ids": [
+      10751,
+      14,
+      35,
+      16
+    ],
+    "backdrop_path": "/m03jul0YdVEOFXEQVUv6pOVQYGL.jpg",
+    "adult": false,
+    "overview": "Dracula, Mavis, Johnny and the rest of the Drac Pack take a vacation on a luxury Monster Cruise Ship, where Dracula falls in love with the ship's captain, Ericka, who's secretly a descendant of Abraham Van Helsing, the notorious monster slayer.",
+    "release_date": "2018-07-12"
+  },
+  {
+    "vote_count": 8507,
+    "id": 102899,
+    "video": false,
+    "vote_average": 7,
+    "title": "Ant-Man",
+    "popularity": 74.472,
+    "poster_path": "/D6e8RJf2qUstnfkTslTXNTUAlT.jpg",
+    "original_language": "en",
+    "original_title": "Ant-Man",
+    "genre_ids": [
+      878,
+      28,
+      12
+    ],
+    "backdrop_path": "/kvXLZqY0Ngl1XSw7EaMQO0C1CCj.jpg",
+    "adult": false,
+    "overview": "Armed with the astonishing ability to shrink in scale but increase in strength, master thief Scott Lang must embrace his inner-hero and help his mentor, Doctor Hank Pym, protect the secret behind his spectacular Ant-Man suit from a new generation of towering threats. Against seemingly insurmountable obstacles, Pym and Lang must plan and pull off a heist that will save the world.",
+    "release_date": "2015-07-14"
+  },
+  {
+    "vote_count": 111,
+    "id": 447200,
+    "video": false,
+    "vote_average": 6,
+    "title": "Skyscraper",
+    "popularity": 73.194,
+    "poster_path": "/3NL1FiTEhU4SWOuWk8lkVCbmnYm.jpg",
+    "original_language": "en",
+    "original_title": "Skyscraper",
+    "genre_ids": [
+      28,
+      53,
+      18
+    ],
+    "backdrop_path": "/oMKFQmoVgB69fyXfSMu0lGlHJP2.jpg",
+    "adult": false,
+    "overview": "Framed and on the run, a former FBI agent must save his family from a blazing fire in the world's tallest building.",
+    "release_date": "2018-07-11"
+  },
+  {
+    "vote_count": 6822,
+    "id": 284054,
+    "video": false,
+    "vote_average": 7.3,
+    "title": "Black Panther",
+    "popularity": 71.834,
+    "poster_path": "/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+    "original_language": "en",
+    "original_title": "Black Panther",
+    "genre_ids": [
+      28,
+      12,
+      14,
+      878
+    ],
+    "backdrop_path": "/b6ZJZHUdMEFECvGiDpJjlfUWela.jpg",
+    "adult": false,
+    "overview": "King T'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne by factions within his own country as well as without. Using powers reserved to Wakandan kings, T'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister, members of the Dora Milaje (the Wakandan 'special forces') and an American secret agent, to prevent Wakanda from being dragged into a world war.",
+    "release_date": "2018-02-13"
+  },
+  {
+    "vote_count": 2820,
+    "id": 238636,
+    "video": false,
+    "vote_average": 6.6,
+    "title": "The Purge: Anarchy",
+    "popularity": 71.172,
+    "poster_path": "/l1DRl40x2OWUoPP42v8fjKdS1Z3.jpg",
+    "original_language": "en",
+    "original_title": "The Purge: Anarchy",
+    "genre_ids": [
+      27,
+      53
+    ],
+    "backdrop_path": "/zWGAnbxjjwY3xFGuOeR26LGbBlG.jpg",
+    "adult": false,
+    "overview": "One night per year, the government sanctions a 12-hour period in which citizens can commit any crime they wish -- including murder -- without fear of punishment or imprisonment. Leo, a sergeant who lost his son, plans a vigilante mission of revenge during the mayhem. However, instead of a death-dealing avenger, he becomes the unexpected protector of four innocent strangers who desperately need his help if they are to survive the night.",
+    "release_date": "2014-07-17"
+  },
+  {
+    "vote_count": 3039,
+    "id": 383498,
+    "video": false,
+    "vote_average": 7.6,
+    "title": "Deadpool 2",
+    "popularity": 70.361,
+    "poster_path": "/to0spRl1CMDvyUbOnbb4fTk3VAd.jpg",
+    "original_language": "en",
+    "original_title": "Deadpool 2",
+    "genre_ids": [
+      28,
+      35,
+      878
+    ],
+    "backdrop_path": "/3P52oz9HPQWxcwHOwxtyrVV1LKi.jpg",
+    "adult": false,
+    "overview": "Wisecracking mercenary Deadpool battles the evil and powerful Cable and other bad guys to save a boy's life.",
+    "release_date": "2018-05-15"
+  },
+  {
+    "vote_count": 3511,
+    "id": 158015,
+    "video": false,
+    "vote_average": 6.1,
+    "title": "The Purge",
+    "popularity": 69.092,
+    "poster_path": "/tGGJOuLHX7UDlTz57sjfhW1qreP.jpg",
+    "original_language": "en",
+    "original_title": "The Purge",
+    "genre_ids": [
+      878,
+      27,
+      53
+    ],
+    "backdrop_path": "/1sZ9Nnic1ldHhHttAMDmNxaNM04.jpg",
+    "adult": false,
+    "overview": "Given the country's overcrowded prisons, the U.S. government begins to allow 12-hour periods of time in which all illegal activity is legal. During one of these free-for-alls, a family must protect themselves from a home invasion.",
+    "release_date": "2013-05-31"
+  }
+]
